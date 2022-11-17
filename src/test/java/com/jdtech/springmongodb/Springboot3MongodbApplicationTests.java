2ecc0a100b4807539f5a2c9bd1006d7e95547562
Joao Diego <joaodiegor@gmail.com>
2022-11-17 10:40:20 -0300

    Primeiro Commit - Projeto Criado

---
+++ src/test/java/com/jdtech/springmongodb/Springboot3MongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdtech.springmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3MongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
