b0d6024712367cf64b96acca90ea5f544d1e6eeb
Joao Diego <joaodiegor@gmail.com>
2022-11-29 11:22:32 -0300

    Classe Instantiation de Configuração criada

---
+++ src/main/java/com/jdtech/springmongodb/config/Instantiation.java
@@ -0,0 +1,28 @@
+package com.jdtech.springmongodb.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.jdtech.springmongodb.domain.User;
+import com.jdtech.springmongodb.repositories.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		userRepository.deleteAll();
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+		
+	}
+
+}
