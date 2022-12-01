ec7343ee09bda353dc596f45cb694b624d7d61d7
Joao Diego <joaodiegor@gmail.com>
2022-12-01 09:41:45 -0300

    FindById com Exceptions criado

--- src/main/java/com/jdtech/springmongodb/services/UserService.java
+++ src/main/java/com/jdtech/springmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.jdtech.springmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jdtech.springmongodb.domain.User;
 import com.jdtech.springmongodb.repositories.UserRepository;
+import com.jdtech.springmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -15,7 +17,12 @@ public class UserService {
 	private UserRepository repo;
 	
 	public List<User> findAll(){
-		return repo.findAll();
+		return repo.findAll();		
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		
 	}
 
