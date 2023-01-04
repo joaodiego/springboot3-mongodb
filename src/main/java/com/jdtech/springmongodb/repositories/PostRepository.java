421b6add2e9f792b29ffbe4ca054a92565db742c
Joao Diego <joaodiegor@gmail.com>
2023-01-04 11:16:34 -0300

    Adicionando consulta simples utilizando @Query

--- src/main/java/com/jdtech/springmongodb/repositories/PostRepository.java
+++ src/main/java/com/jdtech/springmongodb/repositories/PostRepository.java
@@ -3,6 +3,7 @@ package com.jdtech.springmongodb.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.jdtech.springmongodb.domain.Post;
@@ -10,6 +11,7 @@ import com.jdtech.springmongodb.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
 	List<Post> findByTitle(String title);
 
 }
--- src/main/java/com/jdtech/springmongodb/resources/PostResource.java
+++ src/main/java/com/jdtech/springmongodb/resources/PostResource.java
@@ -2,7 +2,6 @@ package com.jdtech.springmongodb.resources;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
-import java.net.URL;
 import java.net.URLDecoder;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
